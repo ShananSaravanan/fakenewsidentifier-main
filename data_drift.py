e08ca06a80e29d548a670acb48b4e7d3845eabc5
Dreshya <120552474+Dreshya@users.noreply.github.com>
2025-01-20 02:41:15 +0800

    Modifying data drift detection
    Changing data drift detection code + sampling the training data checked

--- data_drift.py
+++ data_drift.py
@@ -1,34 +1,30 @@
-from evidently.report import Report
-from evidently.metrics import DataDriftTable
-from evidently import ColumnMapping
 import pandas as pd
+import numpy as np
+from sklearn import datasets, ensemble
+
+from evidently import ColumnMapping
+from evidently.report import Report
+from evidently.metric_preset import DataDriftPreset, TargetDriftPreset, RegressionPreset
 
 def check_data_drift(train_data_path, new_data_path):
+
     # Load training data
     train_data = pd.read_excel(train_data_path)
     new_data = pd.read_excel(new_data_path)
 
-    # Define column mapping (adjust based on your dataset)
-    column_mapping = ColumnMapping(
-        # Specify feature and target columns if necessary
-        prediction=None,
-        target=None,
-        id=None,
-        numerical_features=[],
-        categorical_features=["processed_text","label"]  # Adjust as needed
-    )
-    
-    # Initialize Evidently Report with DataDriftTable metric
-    report = Report(metrics=[DataDriftTable()])
+    train_data_sample = train_data.sample(n=13000) 
+
+    # Initialize the Evidently report
+    drift_report = Report(metrics=[DataDriftPreset()])
     
     # Calculate drift
-    report.run(reference_data=train_data, current_data=new_data, column_mapping=column_mapping)
+    drift_report.run(reference_data=train_data_sample, current_data=new_data)
     
     # Save the drift report to an HTML file
-    report.save_html("data_drift_report.html")
+    drift_report.save_html("data_drift_report.html")
     
     # Optionally, check for dataset drift and return a message
-    drift_metrics = report.as_dict()
+    drift_metrics = drift_report.as_dict()
     if drift_metrics["metrics"][0]["result"]["dataset_drift"]:
         return "Data drift detected. Retraining recommended."
     else:
